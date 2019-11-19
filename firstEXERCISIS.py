794d158270851e586b548652bffb595d2bce3cb7
emad-alrsheed <alrash.emad@gmail.com>
2019-11-19 16:32:05 +0200

    sloving the exercise 1

---
+++ firstEXERCISIS.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 19 13:01:30 2019
+
+@author: emad
+"""
+
+print("HELLO")
+
+print('''hello 
+              hello
+                  hello
+      ''')
+#num 2
+
+print("orang Acadmy"*20)
+
+#3
+x=1
+y=2.8
+z=1j
+o="orange"
+a=True
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(o))
+print(type(a))
+
+#4
+x,y="orange",1
+x1,y1=100,-10
+print(x)
+print(y)
+print(x1)
+print(y1)
+
+
+#5
+x=10
+print(x)
+x_q="orange"
+print(x_q)
+ab=14
+print(ab)
+print("10",10)
+
+
+#6
+#print(hello,word!")
+print("cheers,mate!")#this is program
+"""testing
+the code 
+"""
+
+#7
+x=11
+y=3
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+
+print(x//y)
+print(x%y)
+print(abs(x*-1))
+print(int(x))
+print(float(x))
+
+print(divmod(x,y))
+print(pow(x,y))
+print(x**y)
+print(x>y)
+print(x==y)
+print(x!=y)
+
+
+#13
+
+
+base=10 
+height=20
+Area=1/2*base*height
+print(Area)
+
+#12
+
+name="emad"
+age=15
+Age=1992+100-27
+print(Age)
+#5
+
+a=10
+b=20
+c=15
+d=25
+e=30
+avg=(a+b+c+d+e)/5
+print(avg)
+
+
+#4
+
+product=a*b
+print(product)
